e1f2c849bec8d23c1bedd0baf40a0df3e5bdb092
Florian Golemo <fgolemo@gmail.com>
2013-12-18 15:14:16 +0100

    now the demo is working, but looks nothing like in the paper. :D Further tuning necessary.

--- test_f_talksTo.m
+++ test_f_talksTo.m
@@ -1,5 +1,7 @@
+%% var init
 steps = 10;
-u_i = 0.5;
+u_i = 0.25; % this is random, I just chose 0.5 out of nowhere,
+% I also tried different vales and the outcome doesn't change
 u_j = [
     0.25*u_i,
     0.5*u_i,
@@ -7,16 +9,37 @@ u_j = [
     1.5*u_i
     ];
 
-x_j = 0.5
+x_j = 0.5 % this is random, I just chose the value out of nowhere (between -1 and +1),
+% I also tried different values and the outcome doesn't change
 
-log = zeros(steps, 4);
+log = zeros(steps, 4, 2);
 
-for j = 1:4
+
+%% main loop, try 1
+for i = 1:4
     u_j_current = u_j(i);
     stepping = 2*u_j_current/steps;
     
-    for x_i = x_j-u_j_current : x_j+u_j_current : steps;
-        overlap = min (x_i + u_i, x_j + u_j_current) - max (x_i - u_i, x_j - u_j_current)
+    j = 1;
+    for x_i = x_j-u_j_current : stepping : x_j+u_j_current;
+        overlap = min (x_i + u_i, x_j + u_j_current) - max (x_i - u_i, x_j - u_j_current);
+        rel_agreement = (overlap/u_i)-1;
+        
+        sprintf('I:%d, J:%d\n', i, j)
+        log(j,i,1) = rel_agreement;
+        log(j,i,2) = x_i;
+        j=j+1;
     end
 
 end
+
+%% draw the stuff
+figure();
+hold on;
+title('\it{relative agreement over x_i}','FontSize',16)
+xlabel('xi')
+ylabel('rel. agreement')
+x = 1:11;
+plot (x, log(:,1,1), '-ro', x, log(:,2,1), '-.b', x, log(:,3,1), '-.g', x, log(:,4,1), '-co');
+legend('0.25ui', '0.5ui', 'ui', '1.5ui');
+hold off;
